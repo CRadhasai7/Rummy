d010774ffdf6b901aeec5c4cee1b828d21e988b2
CRadhasai7 <rc1126@wildcats.unh.edu>
2019-04-22 18:01:33 -0400

    added the discard

--- src/Rummy/Discard.java
+++ src/Rummy/Discard.java
@@ -2,4 +2,20 @@ package Rummy;
 
 public class Discard {
 
+	private Card topOfDiscard;
+	
+	public Card getTop(){
+		return this.topOfDiscard;
+	}
+	
+	public void setTop(Card card){
+		this.topOfDiscard = card;
+	}
+	
+	public Card takeTop(){
+		Card temp = topOfDiscard;
+		topOfDiscard = null;
+		return(temp);
+	}
+	
 }
