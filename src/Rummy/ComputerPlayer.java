54be9dc07898c64625505167b77e6279e4a41e9c
CRadhasai7 <rc1126@wildcats.unh.edu>
2019-04-29 17:17:32 -0400

    extended the playerclass to computerplayer

---
+++ src/Rummy/ComputerPlayer.java
@@ -0,0 +1,12 @@
+package Rummy;
+
+public class ComputerPlayer extends Player{
+
+	private int gameTurns = 0;
+	
+	public void setGameTurns(int gameTurns){
+		this.gameTurns = gameTurns;
+	}
+	
+	
+}
